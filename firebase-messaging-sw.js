03cdb71d1b45af84d04ddd68e88bb914c4914628
weesimbkilhamodkalbalhadrany-pixel <weesimbkilhamodkalbalhadrany@gmail.com>
2026-05-03 04:28:51 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,27 +1,23 @@
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCMb07oP2PMUdTlffJjYkbfThC34x1dbtU",
-    authDomain: "waselny-2812b.firebaseapp.com",
-    databaseURL: "https://waselny-2812b-default-rtdb.firebaseio.com",
-    projectId: "waselny-2812b",
-    storageBucket: "waselny-2812b.firebasestorage.app",
-    messagingSenderId: "662536104158",
-    appId: "1:662536104158:web:666db40942313724c3fa4f",
-    measurementId: "G-YY8LPD0RH7"
-};
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+});
 
-firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '/favicon.ico',
-        click_action: payload.notification.click_action || '/'
-    };
-    self.registration.showNotification(notificationTitle, notificationOptions);
+  console.log('إشعار في الخلفية:', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });

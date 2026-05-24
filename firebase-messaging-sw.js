67fb93dc28c30fa306dd485e6d45fc7010a546b8
weesimbkilhamodkalbalhadrany-pixel <weesimbkilhamodkalbalhadrany@gmail.com>
2026-05-24 03:25:14 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,21 @@
-importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBvnmHgIm4Xs-d-LBQo02BTPGxm_FZsrk",
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBvnmHgNIm4Xs-d-LBQo02BTPGxm_FZsrk",
   authDomain: "wassal-app-final.firebaseapp.com",
   projectId: "wassal-app-final",
   storageBucket: "wassal-app-final.firebasestorage.app",
   messagingSenderId: "110183430414",
-  appId: "1:110183430414:web:e4b8e88e31e91a0f61ff5f"
-});
+  appId: "1:110183430414:web:e4b8e88e31e91a0f61ff5f",
+  measurementId: "G-0K3TTX2NMC"
+};
 
-const messaging = firebase.messaging();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

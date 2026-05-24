946c9a90326236323936a61e5edc6c2c1b34065f
weesimbkilhamodkalbalhadrany-pixel <weesimbkilhamodkalbalhadrany@gmail.com>
2026-05-24 03:25:48 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js');
+firebase.initializeApp({
   apiKey: "AIzaSyBvnmHgNIm4Xs-d-LBQo02BTPGxm_FZsrk",
   authDomain: "wassal-app-final.firebaseapp.com",
   projectId: "wassal-app-final",
@@ -14,8 +8,8 @@ const firebaseConfig = {
   messagingSenderId: "110183430414",
   appId: "1:110183430414:web:e4b8e88e31e91a0f61ff5f",
   measurementId: "G-0K3TTX2NMC"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+});
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, { body: payload.notification.body });
+});

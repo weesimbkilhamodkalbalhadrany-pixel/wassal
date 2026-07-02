c80d7fa253d2c1797244f0de99154dcb7871c7e0
weesimbkilhamodkalbalhadrany-pixel <weesimbkilhamodkalbalhadrany@gmail.com>
2026-07-02 04:33:18 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,12 @@
 importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js");
 
-// تهيئة الخدمة بنفس بيانات مشروعك الدقيقة
+// التهيئة بالبيانات الدقيقة والمطابقة لمشروعك ولقاعدة البيانات
 firebase.initializeApp({
     apiKey: "AIzaSyBvmggMwxr-dJi0Q00BTf9cm_FzSrK",
-    authDomain: "wasal-app-final.firebaseapp.com",
-    projectId: "wassal-app-final", // ⚠️ تأكد من كتابتها دبل s هنا لتطابق قاعدة البيانات
-    storageBucket: "wasal-app-final.firebasestorage.app",
+    authDomain: "wassal-app-final.firebaseapp.com",
+    projectId: "wassal-app-final", 
+    storageBucket: "wassal-app-final.firebasestorage.app",
     messagingSenderId: "110183430614",
     appId: "1:110183430614:web:e4b8e83e19a6f1ff5f"
 });
@@ -17,13 +17,11 @@ const messaging = firebase.messaging();
 // استقبال الإشعارات في الخلفية
 messaging.onBackgroundMessage((payload) => {
     console.log('Received background message ', payload);
-
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: "logo.png",
-        badge: "logo.png"
+        icon: "/logo.png",
+        badge: "/logo.png"
     };
-
     self.registration.showNotification(notificationTitle, notificationOptions);
 });

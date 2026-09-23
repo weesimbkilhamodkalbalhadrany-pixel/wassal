378a0ffdd4a002d106fe05558913b817313e8a34
weesimbkilhamodkalbalhadrany-pixel <weesimbkilhamodkalbalhadrany@gmail.com>
2026-09-23 13:08:39 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'tsaheel-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.url.includes('firebaseio.com') ||
+      e.request.url.includes('googleapis.com') ||
+      e.request.url.includes('gstatic.com')) {
+    return;
+  }
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
